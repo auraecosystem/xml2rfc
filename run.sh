b437d4a646933a8903ecffb4e3efee4d18ce8014
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-18 22:01:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker run -it $@ -v ${PWD}:/root/xml2rfc ghcr.io/ietf-tools/xml2rfc-dev:latest
+docker run --rm `# automatically remove container upon termination` \
+  -v "$(pwd):/data" `# bind current working directory to /data` \
+  ghcr.io/ietf-tools/xml2rfc-slim:latest \
+  --html example.xml
+# if input is example.xml, output will be example.html
